ad181705b610560059df4ba56e814ac4cc3cb4e4
Kalman Olah <hello@kalmanolah.net>
2015-12-09 09:58:12 +0100

    Allow specifying of device interfaces, input and output endpoints.
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

--- escpos_cli/__init__.py
+++ escpos_cli/__init__.py
@@ -18,13 +18,22 @@ def main():
     parser = argparse.ArgumentParser(prog='escpos-cli', description='%(prog)s is a command-line utility for printing stuff using pyxmlescpos')
     parser.add_argument('-dvid', '--device-vendor-id', required=True, type=auto_int)
     parser.add_argument('-dpid', '--device-product-id', required=True, type=auto_int)
+    parser.add_argument('-di', '--device-interface', type=int, default=0)
+    parser.add_argument('-die', '--device-input-endpoint', type=auto_int, default=0x82)
+    parser.add_argument('-doe', '--device-output-endpoint', type=auto_int, default=0x01)
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument('-o', '--open-cashdrawer', action='store_true')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __VERSION__)
     parser.add_argument('receipt_xml', type=str)
     args = parser.parse_args()
 
-    device = Usb(args.device_vendor_id, args.device_product_id)
+    device = Usb(
+        args.device_vendor_id,
+        args.device_product_id,
+        interface=args.device_interface,
+        in_ep=args.device_input_endpoint,
+        out_ep=args.device_output_endpoint
+    )
 
     if args.open_cashdrawer:
         device.cashdraw(2)
